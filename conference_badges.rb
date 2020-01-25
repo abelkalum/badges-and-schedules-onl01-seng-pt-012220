f243c1ab251902b4b58bdf7db308b3126fcbe97c
Abel Kalum <chumbabel@gmail.com>
2020-01-25 13:28:30 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,5 +1,5 @@
-# Write your code here.
+people = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
 
 def badge_maker(name)
-   "Hello, my name is #{name}."
+  return "Hello, my name is #{name}."
 end
